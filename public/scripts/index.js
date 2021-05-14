0e97676d38f7ad55e144fa5d1650c135415537e1
euonimus <76622666+euonimus@users.noreply.github.com>
2021-05-14 17:26:26 +0200

    Grundgerüst

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,18 +1,18 @@
-const form = document.getElementById("form");
-const title = document.getElementById("title");
-const description = document.getElementById("description");
-const importance = document.getElementById("importance");
-const duedate = document.getElementById("duedate");
-
-form.addEventListener("submit", event => {
-    event.preventDefault();
-
-    const message = `
-    Title: ${title.value}
-    Description: ${description.value}
-    Importance: ${importance.value}
-    Duedate: ${duedate.value}
-  `;
-
-    alert(message);
-});
+// const form = document.getElementById("form");
+// const title = document.getElementById("title");
+// const description = document.getElementById("description");
+// const importance = document.getElementById("importance");
+// const duedate = document.getElementById("duedate");
+//
+// form.addEventListener("submit", event => {
+//     event.preventDefault();
+//
+//     const message = `
+//     Title: ${title.value}
+//     Description: ${description.value}
+//     Importance: ${importance.value}
+//     Duedate: ${duedate.value}
+//   `;
+//
+//     alert(message);
+// });
