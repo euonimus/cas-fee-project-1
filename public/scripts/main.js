a3023a954513476265dcb96d414a7dc5b2fa9283
euonimus <76622666+euonimus@users.noreply.github.com>
2021-05-17 19:40:39 +0200

    reorg css, select in menu, transition, sortList()

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,3 +6,27 @@ function changeTheme() {
     document.body.classList.toggle("dark-theme", false);
   }
 }
+
+function sortList(id) {
+  if (document.getElementById(id).classList.contains("current")) {
+    //if already set > unset
+    id = undefined;
+  }  
+
+  document.getElementById("by_finished").classList.toggle("current", false);
+  document.getElementById("by_created").classList.toggle("current", false);
+  document.getElementById("by_importance").classList.toggle("current", false);
+
+  switch(id) {
+    case 'by_finished':
+      break;
+    case 'by_created':
+      break;
+    case 'by_importance':
+      break;
+    default:
+      return;
+  }
+
+  document.getElementById(id).classList.toggle("current", true);
+}
